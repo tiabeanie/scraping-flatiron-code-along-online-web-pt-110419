d4728d85336c8d2dc1874384a0ec3869a3ab0422
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-15 22:29:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,12 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end 
+ 
+Scraper.new.get_page
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
